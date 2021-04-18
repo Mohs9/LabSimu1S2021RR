2f325d660031b3dac1e0ca3c3c8b6619e22859a1
root <root@DESKTOP-3E65TPK.localdomain>
2021-04-18 00:15:04 -0600

    problema1.2

--- C/Laboratorio4/problema1.c
+++ C/Laboratorio4/problema1.c
@@ -15,28 +15,37 @@ void print_ascendentemente(int a[], int n);
 void print_descendentemente(int a[], int n);
 void selection_sort(int P[], int n);
 
-    void main()
+void main()
 {   char caracter;
     int n = 10;
     int P[] = {16, 20, 12, 6, 8, 2, 14, 4, 10, 18};
     
     selection_sort(P, n);
 
+    //pide los caracteres hasta que el usuario ponga a o d
+
+    
     do{
         puts("Escoja a o d");
-        caracter = getchar();
-    }while((caracter)!='a' && (caracter)!='d');
+        scanf(" %c", &caracter);
+        if ((caracter) != 'a' && (caracter) != 'd'){
+        puts("Caracter invalido");
+        }
+    }while ((caracter) != 'a' && (caracter) != 'd');
 
-    if(caracter=='a'){
-        puts("vector ordenado ascendentemente");
-        print_ascendentemente(P, n);
-    }
+        //Dependiendo del caracter realiza una funcion
+        if (caracter == 'a')
+        {
+            puts("vector ordenado ascendentemente");
+            print_ascendentemente(P, n);
+        }
 
-    if (caracter =='d')
-    {
-        puts("vector ordenado descendentemente");
-        print_descendentemente(P, n);
-    }
+        else if (caracter == 'd')
+        {
+            puts("vector ordenado descendentemente");
+            print_descendentemente(P, n);
+        }
+    
 }
     
 
@@ -45,17 +54,17 @@ void selection_sort(int P[], int n);
 void selection_sort(int P[], int n){
 
     int min, i, j;
-    //se utiliza el primer espacio del vector para comparar el resto de los valores
+    //se utiliza la primera posicion del vector para comparar el resto de los valores
     for (i = 0; i < n - 1; i++)
     {
         min = i;
-        //se busca en la sublista generada la posicion del menor valor
+        //se busca en la sublista generada la posicion del valor minimo
         for (j = i + 1; j < n; j++)
         {
             if (P[j] < P[min])
                 min = j;
         }
-        //se realiza el cambio del menor elemento por el seleccionado de pivote, solo si este es mayor
+        //se realiza el cambio del minimo por el seleccionado de pivote, solo si este es mayor
         if (i != min)
         {
             int aux = P[i];
@@ -66,7 +75,7 @@ void selection_sort(int P[], int n){
 
 
 }
-
+//imprime el vector del minimo al maximo
 void print_ascendentemente(int a[], int n)
 {
     for (int i = 0; i < n; i++)
@@ -74,7 +83,7 @@ void print_ascendentemente(int a[], int n)
         printf("%d  ", a[i]);
     }
 }
-
+//imprime el vector del maximo al minimo
 void print_descendentemente(int a[], int n)
 {
     for (int i = n-1; i >=0; i--)
